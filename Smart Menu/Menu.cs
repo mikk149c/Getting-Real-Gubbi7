5629b49024909434ef927dd5256f212751bfaffe
Maria Hovgaard Rasmussen <Mari55fe@edu.eal.dk>
2018-11-29 14:31:44 +0100

    Smart Menu Konsol implementering startet.

--- Smart Menu/Menu.cs
+++ Smart Menu/Menu.cs
@@ -1,24 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Smart_Menu
 {
 	public class Menu
 	{
-		private string name;
-		public string Name { get { return name; } set { name = value; } }
-		public Menu(string name)
-		{
-			Name = name;
-		}
+		private string mainMenuName;
+        private List<IMenuPoint> menuPointList = new List<IMenuPoint>();
+        private int pointer = 0;
 
+        public string Name { get { return mainMenuName; } set { mainMenuName = value; } }
+		public Menu(string mainMenuName)
+		{
+			Name = mainMenuName;
+		}
+        
 		public void AddMenuPoint(IMenuPoint menuPoint)
 		{
-			throw new NotImplementedException();
+			menuPointList.Add(menuPoint);
 		}
 
 		public void Activate()
 		{
-			throw new NotImplementedException();
+			Console.Clear();
+            displayCursor();
+
 		}
-	}
+
+        public void displayCursor()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.CursorVisible = false;
+            Console.WriteLine(mainMenuName);
+            Console.WriteLine(menuPointList);
+
+        }
+        //Skriver til Konsollen
+        public void menuPointCount()
+        {
+            for (int i = 0; i < menuPointList.Count; i++)
+            {
+                displaySelect(i);
+            }
+        }
+
+        private void displaySelect(int i)
+        {
+            if (i.Equals(pointer))
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(menuPointList[i].GetMenuPointName());
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.WriteLine(menuPointList[i].GetMenuPointName());
+            }
+            
+        }
+    }
 }
